cf397baf26feb8a1878d39e9a214b1cc80bd685c
Rui Vieira <ruidevieira@googlemail.com>
2025-02-28 10:52:31 +0000

    chore: Add core placeholder

---
+++ src/core/__init__.py
@@ -0,0 +1,4 @@
+# Place all external packages and core algorithms here
+# The main REST server should only call
+# - modules directly related to the service HTTP, storage, prometheus, etc.
+# - This core module
